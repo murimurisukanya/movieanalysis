6bb1af83dc84363e7cb65d99927e73dfe71ce64c
murimurisukanya <113427828+murimurisukanya@users.noreply.github.com>
2023-07-24 18:40:17 +0530

    Create movie_analysis.py

---
+++ movie_analysis.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df_review=pd.read_csv("IMDB Dataset.csv")
+
+df_review.head(10)
+
+df_positive = df_review[df_review['sentiment']=='positive'][:9000]
+df_negative = df_review[df_review['sentiment']=='negative'][:1000]
+
+df_review_imb = pd.concat([df_positive,df_negative ])
+
+colors = sns.color_palette('deep')
+
+plt.figure(figsize=(8,4), tight_layout=True)
+plt.bar(x=['Positive', 'Negative'],
+        height=df_review_imb.value_counts(['sentiment']),
+        color=colors[:2])
+plt.title('Sentiment')
+plt.savefig('sentiment.png')
+plt.show()
+
+from imblearn.under_sampling import RandomUnderSampler
+rus = RandomUnderSampler(random_state= 0)
+df_review_bal,df_review_bal['sentiment']=rus.fit_resample(df_review_imb[['review']],df_review_imb['sentiment'])
+df_review_bal
+
+print(df_review_imb.value_counts('sentiment'))
+print(df_review_bal.value_counts('sentiment'))
+
+
+from sklearn.model_selection import train_test_split
+
+train,test = train_test_split(df_review_bal,test_size =0.33,random_state=42)
+train_x, train_y = train['review'], train['sentiment']
+test_x, test_y = test['review'], test['sentiment']
+
+train_y.value_counts()
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+tfidf = TfidfVectorizer(stop_words='english')
+train_x_vector = tfidf.fit_transform(train_x)
+# also fit the test_x_vector
+test_x_vector = tfidf.transform(test_x)
+
+pd.DataFrame.sparse.from_spmatrix(train_x_vector,
+                                  index=train_x.index,
+                                  columns=tfidf.get_feature_names())
+
+from sklearn.svm import SVC
+svc = SVC(kernel='linear')
+svc.fit(train_x_vector, train_y)
+
+print(svc.predict(tfidf.transform(['A good movie'])))
+print(svc.predict(tfidf.transform(['An excellent movie'])))
+print(svc.predict(tfidf.transform(['I did not like this movie at all I gave this movie away'])))
+
+print(svc.score(test_x_vector, test_y))
+
+
+from sklearn.metrics import f1_score
+
+f1_score(test_y,svc.predict(test_x_vector),
+          labels = ['positive','negative'],average=None)
+
+from sklearn.metrics import classification_report
+
+print(classification_report(test_y,
+                            svc.predict(test_x_vector),
+                            labels = ['positive','negative']))
+
+from sklearn.metrics import confusion_matrix
+
+conf_mat = confusion_matrix(test_y,
+                           svc.predict(test_x_vector),
+                           labels = ['positive', 'negative'])
+conf_mat
+
+
+from sklearn.model_selection import GridSearchCV
+#set the parameters
+params = {'C': [1,4,8,16,32], 'kernel' : ['linear','rbf']}
+svc = SVC()
+svc_grid = GridSearchCV(svc,params, cv = 5)
+svc_grid.fit(train_x_vector, train_y)
+
+print(svc_grid.best_params_)
+print(svc_grid.best_estimator_)
